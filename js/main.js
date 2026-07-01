a9dbc87132693006e59258c291b554bf9954f0ba
vannt <ngthivan2222@gmail.com>
2026-07-01 19:01:18 +0700

    feat: header and footer

---
+++ js/main.js
@@ -0,0 +1,23 @@
+ const navLinks = document.querySelectorAll('.nav__link');
+ const menuBtn = document.querySelector(".mobile-menu-btn");
+const mobileMenu = document.querySelector(".mobile-menu");
+const closeBtn = document.querySelector(".mobile-menu__close");
+
+
+navLinks.forEach((nav)=>{
+    nav.addEventListener('click',(event)=>{
+      mobileMenu.classList.remove("active");
+      navLinks.forEach(link=>link.classList.remove('active'))
+      nav.classList.add('active');
+    })
+  })
+
+
+menuBtn.addEventListener("click", () => {
+  mobileMenu.classList.add("active");
+});
+
+closeBtn.addEventListener("click", () => {
+  mobileMenu.classList.remove("active");
+});
+
